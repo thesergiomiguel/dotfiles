0a0e3590e45c7ebe458546fc1bdd28de304888e5
Sérgio Miguel <thesergiomiguel@gmail.com>
2024-11-01 11:29:16 +0100

    feat(zsh): navigate suggestions using M-j/M-k

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -11,8 +11,8 @@ ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(history-beginning-search-backward-end history-be
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
-bindkey "^[[A" history-beginning-search-backward-end
-bindkey "^[[B" history-beginning-search-forward-end
+bindkey "^[k" history-beginning-search-backward-end
+bindkey "^[j" history-beginning-search-forward-end
 
 source "$ZDOTDIR/vendor/zsh-autosuggestions/zsh-autosuggestions.zsh"
 

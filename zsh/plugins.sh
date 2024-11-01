3a6d5d4aff04c37a6bb99b3a6200ec12eef267ad
Sérgio Miguel <thesergiomiguel@gmail.com>
2024-11-01 10:50:24 +0100

    fix: autosuggestions

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -4,6 +4,16 @@
 source "$ZDOTDIR/vendor/zsh-z/zsh-z.plugin.zsh"
 
 # Autosuggestions
+export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(history-beginning-search-backward-end history-beginning-search-forward-end)
+
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
+
 source "$ZDOTDIR/vendor/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
 # Syntax highlighting

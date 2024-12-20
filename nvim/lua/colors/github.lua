503885510d5f9bc7a61ec81a5ec8a0e9061a5685
Sérgio Miguel <thesergiomiguel@gmail.com>
2024-12-20 13:51:10 +0100

    nvim: overwrite gh colorschemes highlight groups

--- nvim/lua/colors/github.lua
+++ nvim/lua/colors/github.lua
@@ -3,11 +3,32 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    local base = '#2f353a'
+
+    local c = {
+      bg = {
+        border = base,
+        title = '#aa5b55',
+        normal = base,
+        counter = 'fg1',
+        cursor_line = '#40474e',
+        bqf_thumb = '#aa5b55',
+      },
+
+      fg = {
+        border = base,
+        title = 'fg1',
+        normal = 'fg1',
+      },
+    }
+
     require('github-theme').setup {
       options = {
+        transparent = true,
+
         styles = {
           comments = 'italic',
-          -- functions = 'italic',
+          functions = 'italic',
         },
 
         darken = {
@@ -21,10 +42,33 @@ return {
       groups = {
         all = {
           FlashLabel = { fg = 'fg1', bg = 'sel1' },
+
+          TelescopePromptBorder = { bg = c.bg.border, fg = c.fg.border },
+          TelescopePromptTitle = { bg = c.bg.title, fg = c.fg.title },
+          TelescopePromptNormal = { bg = c.bg.normal, fg = c.fg.normal },
+          TelescopePromptCounter = { fg = c.bg.counter },
+          TelescopeResultsBorder = { bg = c.bg.border, fg = c.fg.border },
+          TelescopeResultsTitle = { bg = c.bg.title, fg = c.fg.title },
+          TelescopeResultsNormal = { bg = c.bg.normal },
+          TelescopePreviewBorder = { bg = c.bg.border, fg = c.fg.border },
+          TelescopePreviewTitle = { bg = c.bg.title, fg = c.fg.title },
+          TelescopePreviewNormal = { bg = c.bg.normal },
+
+          CursorLine = { bg = c.bg.cursor_line },
+
+          BqfPreviewFloat = { bg = c.bg.normal },
+          BqfPreviewBorder = { bg = c.bg.border, fg = c.fg.border },
+          BqfPreviewThumb = { bg = c.bg.bqf_thumb },
+
+          WhichKey = { bg = c.bg.border },
+          WhichKeyNormal = { bg = c.bg.border },
+          WhichKeyBorder = { bg = c.bg.border, fg = c.fg.border },
+          WhichKeyTitle = { bg = c.bg.title, fg = c.fg.titl },
         },
 
         github_dark = {
-          WhichKeyNormal = { link = 'Normal' },
+          StatusLine = { link = '@string.regex' },
+          StatusLineNC = { link = '@comment' },
         },
       },
     }

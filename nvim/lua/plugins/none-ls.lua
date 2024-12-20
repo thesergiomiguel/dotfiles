8db87c34f3420399384e8967621939aba6de5a44
Sérgio Miguel <thesergiomiguel@gmail.com>
2024-12-20 13:54:12 +0100

    nvim: fix issue with format on save

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -26,16 +26,14 @@ return {
         end, { buffer = bufnr, desc = 'Format buffer' })
 
         --- Format on save
-        if client.supports_method 'textDocument/formatting' then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format { async = false }
-            end,
-          })
-        end
+        vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format { async = false }
+          end,
+        })
       end,
     }
   end,

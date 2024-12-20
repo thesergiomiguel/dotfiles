ec45cc09cef39064e24804b6c395fc50f4553029
Sérgio Miguel <thesergiomiguel@gmail.com>
2024-12-20 13:52:02 +0100

    nvim: tweak symbols' config

--- nvim/lua/plugins/symbols.lua
+++ nvim/lua/plugins/symbols.lua
@@ -8,9 +8,12 @@ return {
         hide_cursor = false,
         show_inline_details = true,
         auto_peek = true,
+        auto_resize = {
+          enabled = true,
+        },
       },
     })
 
-    vim.keymap.set('n', '<localleader>s', '<cmd>SymbolsToggle<CR>')
+    vim.keymap.set('n', '<localleader>s', '<cmd>SymbolsToggle<cr>')
   end,
 }
